60a0ca7425e984052517d474ebe673ec4bbdde26
ephie oyedoh <ephieo@rocketmail.com>
2020-07-01 16:19:47 +0100

    addin output/input boxes and creating an array of objects for the buttons

---
+++ calc2.js
@@ -0,0 +1,100 @@
+const inputElement = document.querySelector ('.input');
+const outputOperationElement = document.querySelector('.operation .value');
+const outputResultElement = document.querySelector('.result .value');
+
+
+let calcButtons = [
+{
+    name:'delete',
+    symbol:'DEL',
+    formula:false,
+    type:'key'
+},
+{
+    name:'',
+    symbol:'',
+    formula:false,
+    type:'key'
+},
+{
+    name:'',
+    symbol:'',
+    formula:false,
+    type:'key'
+},
+{
+    name:'',
+    symbol:'',
+    formula:false,
+    type:'key'
+},
+{
+    name:'',
+    symbol:'',
+    formula:false,
+    type:'key'
+},
+{
+    name:'',
+    symbol:'',
+    formula:false,
+    type:'key'
+},
+{
+    name:'',
+    symbol:'',
+    formula:false,
+    type:'key'
+},
+{
+    name:'',
+    symbol:'',
+    formula:false,
+    type:'key'
+},
+{
+    name:'',
+    symbol:'',
+    formula:false,
+    type:'key'
+},
+{
+    name:'',
+    symbol:'',
+    formula:false,
+    type:'key'
+},
+{
+    name:'',
+    symbol:'',
+    formula:false,
+    type:'key'
+},
+{
+    name:'',
+    symbol:'',
+    formula:false,
+    type:'key'
+},
+{
+    name:'',
+    symbol:'',
+    formula:false,
+    type:'key'
+},
+{
+    name:'',
+    symbol:'',
+    formula:false,
+    type:'key'
+},
+{
+    name:'',
+    symbol:'',
+    formula:false,
+    type:'key'
+},
+
+
+
+];
